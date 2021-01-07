4526506f2bec6cdac6922037c86220ac6237f8da
settleformore <ssettle93@gmail.com>
2021-01-07 13:11:10 +0000

    further styling on About

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Nav from '../components/Nav'
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import styles from '../styles/Home.module.scss'
 
 // import styles from '../styles/Home.module.scss'
 
@@ -7,8 +10,14 @@ class About extends React.Component {
     render(){
         return(
             <>
-                <Nav />
-                <h1>About</h1>
+                <header className={styles.header}>
+                    <h1 className={styles.title}>Randofy</h1>
+                    <nav className={styles.mainnav}>
+                        <Link href='/'>Go Back</Link>
+                    </nav>
+                </header>
+                <div>
+                </div>
             </>
         )
     }
