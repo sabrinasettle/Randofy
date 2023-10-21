31ffaa671886de76e0e9a6cfcb03d80e8d288179
Samuel Oliveira <samuel.oliveira@tevpro.com>
2023-10-21 12:07:07 -0500

    convert everything to latest next, add api from randofy backend

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from '@material-ui/core';
+import Link from '@mui/material/Link';
 
 class NavLink extends React.Component {
-    render() {
-        return (
-            <Link href= {this.props.location}>
-                {this.props.text}
-            </Link>
-        )
-    }
-};
+	render() {
+		return <Link href={this.props.location}>{this.props.text}</Link>;
+	}
+}
 
 export default NavLink;
--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Nav from './Nav';
-import Link from '@material-ui/core/Link';
+import Link from '@mui/material/Link';
 import styles from '../styles/Header.module.scss';
 
 class Header extends React.Component {
-    render(){
-        // console.log(this.props.spotifyUser, "header")
-        return(
-            <header className={styles.header}>
-                <h1 className={styles.title}><Link href='/'>Randofy</Link></h1>
-                <Nav spotifyUser={this.props.spotifyUser} />
-            </header>
-        )
-    }
+	render() {
+		// console.log(this.props.spotifyUser, "header")
+		return (
+			<header className={styles.header}>
+				<h1 className={styles.title}>
+					<Link href="/">Randofy</Link>
+				</h1>
+				<Nav spotifyUser={this.props.spotifyUser} />
+			</header>
+		);
+	}
 }
 
 export default Header;
