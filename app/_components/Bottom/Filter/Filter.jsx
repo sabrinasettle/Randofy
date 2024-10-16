e21c0f0d3ed6f2782bbb2c21bca7ddb12f9637e3
Sabrin Settle <sabrinsettle@Sabrins-MacBook-Air.local>
2024-10-16 17:42:35 -0500

    feat(filterslider): built custom slider

--- app/_components/Bottom/Filter/Filter.jsx
+++ app/_components/Bottom/Filter/Filter.jsx
@@ -1,7 +1,8 @@
 "use client";
-import styles from "../Bottom.module.scss";
 import { useState } from "react";
-
+import { XIcon } from "lucide-react";
+import FilterSlider from "../../Slider/FilterSlider";
+import styles from "../Bottom.module.scss";
 // filter box is fixed, z-index 50,
 // button is absolute
 export default function Filter() {
@@ -13,16 +14,92 @@ export default function Filter() {
     setIsOpen(!isOpen);
   }
 
+  let defaultFilters = {
+    numberOfSongs: 5,
+    popularity: [0, 100],
+    acousticness: [0.0, 1.0], //float range from 0.0 to 1.0
+    danceability: [0.0, 1.0], //float range from 0.0 to 1.0
+    energy: [0.0, 1.0], //float range from 0.0 to 1.0
+    tempo: [0.0, 1.0], //float range from 0.0 to 1.0
+    valence: [0.0, 1.0], //float range from 0.0 to 1.0
+    speechiness: [0.0, 1.0], //float range from 0.0 to 1.0
+    market: "",
+  };
+
   return (
     <>
       {isOpen && (
         <div className={styles["filter-modal"]}>
-          <div>
-            <input type="range" id="volume" name="volume" min="0" max="11" />
-            <label for="volume">Volume</label>
+          <div className="close-modal" onClick={handleOpen}>
+            <XIcon />
           </div>
+          <FilterSlider
+            label="Number of Songs"
+            min={5}
+            max={100}
+            id="number-of-songs"
+            isRange={false}
+            rangeLabels={[5, 25, 50, 75, 100]}
+          />
+          <FilterSlider
+            label="Acoustics"
+            min={0}
+            max={100}
+            id="acoustics"
+            isRange="true"
+            rangeLabels={["no acoutics", "all the acoutics"]}
+          />
+          <FilterSlider
+            label="Energy"
+            min={0}
+            max={100}
+            id="energy"
+            isRange="true"
+            rangeLabels={["no acoutics", "all the acoutics"]}
+          />
+          <FilterSlider
+            label="Danceable"
+            min={0}
+            max={100}
+            id="dance-dance"
+            isRange="true"
+            rangeLabels={["no acoutics", "all the acoutics"]}
+          />
+          <FilterSlider
+            label="Mood"
+            min={0}
+            max={100}
+            id="mood"
+            isRange="true"
+            rangeLabels={["no acoutics", "all the acoutics"]}
+          />
+          <FilterSlider
+            label="Popularity"
+            min={0}
+            max={100}
+            id="popularity"
+            isRange="true"
+            rangeLabels={["no acoutics", "all the acoutics"]}
+          />
+          <FilterSlider
+            label="Tempo"
+            min={0}
+            max={100}
+            id="tempo"
+            isRange="true"
+            rangeLabels={["no acoutics", "all the acoutics"]}
+          />
+          <FilterSlider
+            label="Vocals"
+            min={0}
+            max={100}
+            id="vocals"
+            isRange="true"
+            rangeLabels={["no acoutics", "all the acoutics"]}
+          />
         </div>
       )}
+
       <button
         id={styles["filter-btn"]}
         className={
---
+++ app/_components/Slider/FilterSlider.jsx
@@ -0,0 +1,117 @@
+"use client";
+import { useState } from "react";
+import Slider, { Range } from "rc-slider";
+import styles from "./Slider.module.scss";
+import "rc-slider/assets/index.css";
+
+export default function FilterSlider({
+  id,
+  label,
+  isRange,
+  rangeLabels,
+  min,
+  max,
+}) {
+  const [range, setRange] = useState({ min: min, max: max });
+  // const [values, setValues] = useState([range.min, range.max]);
+  const step = 25;
+  const defaultValue = !isRange ? min : [range.min, range.max];
+
+  console.log(range, defaultValue);
+
+  function changeValues() {}
+
+  const handleMinChange = (e) => {
+    e.preventDefault();
+    const value = parseFloat(e.target.value);
+    const newMinVal = Math.min(value, range.max - step);
+    // if (value <= range.max - step) {
+    setRange({ ...range, min: newMinVal });
+    // }
+  };
+
+  const handleMaxChange = (e) => {
+    e.preventDefault();
+    const value = parseFloat(e.target.value);
+    const newMaxVal = Math.max(value, range.min + step);
+    // if (value <= range.min + step) {
+    setRange({ ...range, max: newMaxVal });
+    // }
+  };
+
+  const minPos = ((range.min - min) / (max - min)) * 100;
+  const maxPos = ((range.max - min) / (max - min)) * 100;
+
+  console.log(minPos, maxPos, range.min, range.max);
+
+  return (
+    <div className={styles["range-container"]} x>
+      {/* Change type of input here for a custom range */}
+      <label className={styles["range-label"]} for={id}>
+        {label}
+      </label>
+      {isRange ? (
+        <div className={styles.wrapper}>
+          <div className={styles["input-wrapper"]}>
+            <input
+              className={`${styles.slider} min`}
+              type="range"
+              id={id}
+              name={id}
+              min={min}
+              max={max}
+              step={25}
+              onChange={handleMinChange}
+              value={range.min}
+            />
+            <input
+              className={`${styles.slider} max`}
+              type="range"
+              id={id}
+              name={id}
+              min={min}
+              max={max}
+              step={25}
+              onChange={handleMaxChange}
+              value={range.max}
+            />
+          </div>
+          <div className={styles["control-wrapper"]}>
+            <div className={styles.control} style={{ left: `${minPos}%` }} />
+            <div className={styles.dot} style={{ left: "25%" }} />
+            <div className={styles.dot} style={{ left: "50%" }} />
+            <div className={styles.dot} style={{ left: "75%" }} />
+            <div className={styles.rail}>
+              <div
+                className={styles["inner-rail"]}
+                style={{ left: `${minPos}%`, right: `${100 - maxPos}%` }}
+              />
+            </div>
+            <div className={styles.control} style={{ left: `${maxPos}%` }} />
+          </div>
+        </div>
+      ) : (
+        // <Slider
+        //   range
+        //   id={id}
+        //   min={range.min}
+        //   max={range.max}
+        //   defaultValue={defaultValue}
+        //   onChange={changeValues}
+        //   step={25}
+        //   dots={true}
+        //   allowCross={false}
+        // />
+        <Slider
+          id={id}
+          min={range.min}
+          max={range.max}
+          defaultValue={defaultValue}
+          onChange={changeValues}
+          step={25}
+          dots={true}
+        />
+      )}
+    </div>
+  );
+}
