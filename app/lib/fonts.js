12df7dcd797f3b8434ad09c302fe5969711ba2f6
Sabrin Settle <sabrinasettle.for.more@gmail.com>
2025-06-25 16:11:21 -0500

    fix(fonts): removed preload

--- app/lib/fonts.js
+++ app/lib/fonts.js
@@ -3,14 +3,12 @@ import { Inclusive_Sans, Hanken_Grotesk } from "next/font/google";
 export const inclusive_sans_init = Inclusive_Sans({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
-  preload: "false",
   variable: "--font-inclusive_sans",
 });
 
 export const hanken_grotesk_init = Hanken_Grotesk({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
-  preload: "true",
   variable: "--font-hanken_grotesk",
 });
 
