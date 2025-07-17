a5ad2a8ef6fa8b3915f56c1bc3e05da1bd3b9f3a
Sabrin Settle <sabrinasettle.for.more@gmail.com>
2025-07-16 19:20:06 -0500

    fix(home): carosuel disappears after choosing filters in mobile

--- app/_components/main_page/SongList/AlbumCarousel.jsx
+++ app/_components/main_page/SongList/AlbumCarousel.jsx
@@ -323,7 +323,7 @@ export default function AlbumCarousel({ songs, onIndexChange }) {
 
   return (
     <div className="flex justify-center">
-      <p className="text-gray-700">{isLoading && "Loading..."}</p>
+      <p className="text-gray-000">{isLoading && "Loading..."}</p>
       <div
         ref={mountRef}
         className="flex items-center justify-center w-full h-[38vh] lg:h-[45vh] bg-transparent overflow-hidden relative"
